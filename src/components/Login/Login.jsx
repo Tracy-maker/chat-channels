fc102ef4e753635c03d314e40adf6f78c8609216
Tracy-maker <ydlvns@gmail.com>
2024-08-22 23:47:24 +1000

    finish

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,6 +9,7 @@ const LoginContainer = styled.div`
   height: 100vh;
   display: grid;
   place-items: center;
+  padding: 20px; // Add some padding for smaller screens
 `;
 
 const LoginInnerContainer = styled.div`
@@ -30,6 +31,36 @@ const LoginInnerContainer = styled.div`
     background-color: #0a8d48 !important;
     color: white;
   }
+
+  // Media query for mobile devices
+  @media (max-width: 768px) {
+    padding: 50px; // Reduce padding on smaller screens
+    width: 100%; // Make the inner container take up full width
+    border-radius: 5px; // Reduce border radius for smaller screens
+
+    > img {
+      height: 80px; // Reduce image size on mobile
+      margin-bottom: 20px; // Reduce margin on mobile
+    }
+
+    > button {
+      margin-top: 30px; // Reduce button margin on mobile
+    }
+  }
+
+  @media (max-width: 480px) {
+    padding: 30px; // Further reduce padding on very small screens
+    width: 90%; // Allow some breathing room on the sides
+
+    > img {
+      height: 60px; // Further reduce image size on very small screens
+      margin-bottom: 10px; // Reduce margin on very small screens
+    }
+
+    > button {
+      margin-top: 20px; // Reduce button margin on very small screens
+    }
+  }
 `;
 
 function Login() {
@@ -46,7 +77,7 @@ function Login() {
       <LoginInnerContainer>
         <img
           src="https://cdn0.iconfinder.com/data/icons/constructivism-for-the-bank/64/constr_support_chat-512.png"
-          alt=""
+          alt="Chat Channels Logo"
         />
         <h1>Sign in to the Chat Channels</h1>
         <p>Chat_together.chatChannels.com</p>
